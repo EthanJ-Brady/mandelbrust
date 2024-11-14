609e084fef8b334c2ecf9f3af5473cd05928fd91
Ethan Brady <git@ethanbrady.xyz>
2024-11-13 21:24:14 -0700

    created a command line argument to select the color

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,24 @@ struct Args {
     #[arg(short='W', long="width", default_value_t = 1920)]
     width: u32,
     #[arg(short='H', long="height", default_value_t = 1080)]
-    height: u32
+    height: u32,
+    #[arg(short='C', long="color", default_value = "ff0000")]
+    color: String,  // Accepts the color as a string in "rrggbb" format
+}
+
+fn parse_color(hex: &str) -> Rgb<u8> {
+    let r = u8::from_str_radix(&hex[0..2], 16).expect("Invalid red component");
+    let g = u8::from_str_radix(&hex[2..4], 16).expect("Invalid green component");
+    let b = u8::from_str_radix(&hex[4..6], 16).expect("Invalid blue component");
+    Rgb([r, g, b])
 }
 
 fn main() {
     let args = Args::parse();
+    let color = parse_color(&args.color);
 
     // Create a new image buffer with the specified dimensions
     let mut img = RgbImage::new(args.width, args.height);
-
-    // Define the color you want to fill, in this case, solid red
-    let color = Rgb([255, 0, 0]); // RGB for red
 
     // Fill the image with the color
     for pixel in img.pixels_mut() {
