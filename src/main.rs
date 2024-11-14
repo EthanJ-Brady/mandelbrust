ebbabd78e5ed77204c46f269bf77e9734fb8f634
Ethan Brady <git@ethanbrady.xyz>
2024-11-13 21:14:17 -0700

    setup command line arguments for height and width

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
+use clap::Parser;
 use image::{Rgb, RgbImage};
 
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short='W', long="width", default_value_t = 1920)]
+    width: u32,
+    #[arg(short='H', long="height", default_value_t = 1080)]
+    height: u32
+}
+
 fn main() {
-    // Define image dimensions
-    let width = 256;
-    let height = 256;
+    let args = Args::parse();
 
     // Create a new image buffer with the specified dimensions
-    let mut img = RgbImage::new(width, height);
+    let mut img = RgbImage::new(args.width, args.height);
 
     // Define the color you want to fill, in this case, solid red
     let color = Rgb([255, 0, 0]); // RGB for red
