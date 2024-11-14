5a4f01b22d397410a8d6170c0a92ae50e126d837
Ethan Brady <git@ethanbrady.xyz>
2024-11-14 00:47:31 -0700

    added a command line parameter to set the background color

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod mandelbrot;
 
 use clap::Parser;
 use color::{parse_color, interpolate_color};
-use image::{Rgb, RgbImage};
+use image::RgbImage;
 use mandelbrot::mandelbrot;
 
 #[derive(Parser, Debug)]
@@ -13,8 +13,10 @@ struct Args {
     width: u32,
     #[arg(short = 'H', long = "height", default_value_t = 1080)]
     height: u32,
-    #[arg(short = 'C', long = "color", default_value = "ff0000")]
+    #[arg(short = 'C', long = "color", default_value = "000000")]
     color: String,  // Accepts the color as a string in "rrggbb" format
+    #[arg(short = 'b', long = "background", default_value = "ffffff")]
+    background: String,  // Accepts the background color as a string in "rrggbb" format
     #[arg(short = 'o', long = "output", default_value = "mandelbrot.png")]
     output: String,
     #[arg(short = 'm', long = "max-iter", default_value_t = 255)]
@@ -24,6 +26,7 @@ struct Args {
 fn main() {
     let args = Args::parse();
     let color = parse_color(&args.color);
+    let background = parse_color(&args.background);
     let mut img = RgbImage::new(args.width, args.height);
 
     let scale_x = 3.5 / args.width as f64;  // Scale width to the Mandelbrot range
@@ -39,7 +42,7 @@ fn main() {
 
         // Map iteration count to color intensity
         let intensity = ((args.max_iter - iter) * 255 / args.max_iter) as u8;
-        let color = interpolate_color(color, Rgb([0, 0, 0]), intensity as f64 / 255.0);
+        let color = interpolate_color(background, color, intensity as f64 / 255.0);
 
         *pixel = color;
     }
