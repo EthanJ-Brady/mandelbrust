3cc46196df66ba4ee528bef8b0a1040d8958c889
Ethan Brady <git@ethanbrady.xyz>
2024-11-14 22:31:29 -0700

    Formatted the project, updated readme, and added another fractal type (#4)

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Mandelbrust
 
+Mandelbrust is a command line application written in Rust which generates images of fractals with a variety of fractals, colors, resolutions, and options.
+
 ## TODO
 
 - [ ] Automatically make the 'o' option determine if the argument provided if a file or a directory
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 mod color;
-mod mandelbrot;
+mod fractal;
 
 use clap::Parser;
-use color::{parse_color, interpolate_color};
+use color::{interpolate_color, parse_color};
+use fractal::{burning_ship, mandelbrot};
 use image::RgbImage;
-use mandelbrot::mandelbrot;
 use std::path::Path;
 
 #[derive(Parser, Debug)]
@@ -24,6 +24,8 @@ struct Args {
     directory: String,
     #[arg(short = 'm', long = "max-iter", default_value_t = 255)]
     max_iter: u32,
+    #[arg(short = 'f', long = "fractal", default_value = "mandelbrot")]
+    fractal: String,
     #[arg(short = 'x', long = "center_x", default_value_t = -0.5, value_parser = clap::value_parser!(f64))]
     center_x: f64,
     #[arg(short = 'y', long = "center_y", default_value_t = 0.0, value_parser = clap::value_parser!(f64))]
@@ -35,12 +37,29 @@ struct Args {
 fn get_filename(args: &Args) -> String {
     let filename = args.output.clone().unwrap_or_else(|| {
         format!(
-            "mandelbrot-x={:.2}-y={:.2}-z={:.2}-{}x{}.png",
-            args.center_x, args.center_y, args.zoom, args.width, args.height
+            "{}-x={:.2}-y={:.2}-z={:.2}-{}x{}.png",
+            args.fractal, args.center_x, args.center_y, args.zoom, args.width, args.height
         )
     });
 
-    Path::new(&args.directory).join(filename).to_string_lossy().to_string()
+    Path::new(&args.directory)
+        .join(filename)
+        .to_string_lossy()
+        .to_string()
+}
+
+fn select_fractal_function(fractal_name: &str) -> fn(f64, f64, u32) -> u32 {
+    match fractal_name {
+        "mandelbrot" => mandelbrot,
+        "burning_ship" => burning_ship,
+        _ => {
+            eprintln!(
+                "Unknown fractal type: {}. Using mandelbrot by default.",
+                fractal_name
+            );
+            mandelbrot
+        }
+    }
 }
 
 fn main() {
@@ -49,11 +68,15 @@ fn main() {
     let background = parse_color(&args.background);
     let mut img = RgbImage::new(args.width, args.height);
 
-    for (x, y, pixel) in img.enumerate_pixels_mut() {
-        let cx = args.center_x + (2.0 * x as f64 - args.width as f64) / (args.height as f64 * args.zoom);
-        let cy = args.center_y + (2.0 * y as f64 - args.height as f64) / (args.height as f64 * args.zoom);
+    let fractal_func = select_fractal_function(&args.fractal);
 
-        let iter = mandelbrot(cx, cy, args.max_iter);
+    for (x, y, pixel) in img.enumerate_pixels_mut() {
+        let cx =
+            args.center_x + (2.0 * x as f64 - args.width as f64) / (args.height as f64 * args.zoom);
+        let cy = args.center_y
+            + (2.0 * y as f64 - args.height as f64) / (args.height as f64 * args.zoom);
+
+        let iter = fractal_func(cx, cy, args.max_iter);
         let intensity = ((args.max_iter - iter) * 255 / args.max_iter) as u8;
         let color = interpolate_color(background, color, intensity as f64 / 255.0);
 
